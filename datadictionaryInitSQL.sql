6e036e0fa09fda81985fcb64fbe678ce8c6f7e5b
DevencD <997595354@qq.com>
2019-07-23 20:26:14 +0800

    添加数据字典初始化脚本

---
+++ datadictionaryInitSQL.sql
@@ -0,0 +1,10 @@
+
+insert into T_DataDictionary (ID, KEY, VALUE, DESCRIPTION)
+values ('37019f04f4d64fc0acfc5785d2d6dabdDATADICTIONARTID', 'openofficeIP', '192.168.182.128', 'openoffice服务器IP');
+
+insert into T_DataDictionary (ID, KEY, VALUE, DESCRIPTION)
+values ('ff74817a32444ecfa41d808d7a2be0e1DATADICTIONARTID', 'openofficePort', '8100', 'openoffice服务器端口');
+
+insert into T_DataDictionary (ID, KEY, VALUE, DESCRIPTION)
+values ('5bbf29e1546d4009a4cba41ef7354dcfDATADICTIONARTID', 'filePerSize', '1500', 'Get请求上传文件，每个文件块的大小（文件流转换为了Base64编码的字符串）');
+
