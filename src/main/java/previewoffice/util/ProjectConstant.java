372818241a410bbc3bc6e188c866c1f7d723c8c6
BC698A630E6F220581C7565359A9A720 <BC698A630E6F220581C7565359A9A720@i-search.com.cn>
2019-10-16 10:05:10 +0800

    操作记录列表优化

--- src/main/java/previewoffice/util/ProjectConstant.java
+++ src/main/java/previewoffice/util/ProjectConstant.java
@@ -6,14 +6,14 @@ public class ProjectConstant
 
     public static String PDFPATH = "";
 
-    public static final String ATTACHMENTID_SUFFIX = "ATTACHMENTID";
+    public static final String ATTACHMENTID_SUFFIX = "-ATTACHMENTID";
 
-    public static final String ATTACHMENTPARTID_SUFFIX = "ATTACHMENTPARTID";
+    public static final String ATTACHMENTPARTID_SUFFIX = "-ATTACHMENTPARTID";
 
-    public static final String DATADICTIONARTID_SUFFIX = "DATADICTIONARTID";
+    public static final String DATADICTIONARTID_SUFFIX = "-DATADICTIONARTID";
     
-    public static final String OPERRECORDID_SUFFIX = "OPERRECORDID";
+    public static final String OPERRECORDID_SUFFIX = "-OPERRECORDID";
     
-    public static final String LOGINNAMEWHITELISTID_SUFFIX = "LOGINNAMEWHITELISTID";
+    public static final String LOGINNAMEWHITELISTID_SUFFIX = "-LOGINNAMEWHITELISTID";
 
 }
